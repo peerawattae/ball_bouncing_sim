4ecd87c1def5bc5acc17c5324b7d6d49e9a22d7b
Peerawat Theerasakul <peerawat.thee@ku.th>
2023-11-20 11:58:53 +0700

    change ball to oop style and change run ball to call it

--- run_ball.py
+++ run_ball.py
@@ -1,11 +1,28 @@
-import turtle
-import ball
 
-num_balls = int(input("Number of balls to simulate: "))
+from ball import Ball
+import turtle
+
+num_balls = int(input("Enter number of ball to sim: "))
 turtle.speed(0)
 turtle.tracer(0)
 turtle.hideturtle()
+turtle.colormode(255)
+
 canvas_width = turtle.screensize()[0]
+canvas_height = turtle.screensize()[1]
+ball_radius = 20
+
+balls = [Ball(canvas_width, canvas_height, ball_radius) for _ in range(num_balls)]
+
+while True:
+        turtle.clear()
+
+        for ball in balls:
+            ball.move(canvas_width, canvas_height)
+            ball.draw()
+
+        turtle.update()
+
 canvas_height = turtle.screensize()[1]
 ball_radius = 0.05 * canvas_width
 turtle.colormode(255)
